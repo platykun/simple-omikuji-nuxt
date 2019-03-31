036dcfc77e3c3c2d7cd9aba090e1e7a31edbe401
platykun <nagonago561@yahoo.co.jp>
2019-03-31 19:23:03 +0900

    追加ボタン実装

--- pages/index.vue
+++ pages/index.vue
@@ -5,62 +5,72 @@
       <br>http://localhost:3000/?users=nannany,platykun
     </dev>
     <br>
-    <v-container fluid>
-      <v-flex v-for="(user,index) in users" :key="index">
-        <v-checkbox v-model="selected" :label="user" :value="user">{{user}}</v-checkbox>
-      </v-flex>
-    </v-container>
-    <v-btn @click="shuffle">シャッフルボタン</v-btn>
+    <v-btn fab dark color="info">
+      <v-icon dark @click="addUser">add</v-icon>
+    </v-btn>
+
+    <v-list light two-line>
+      <template v-for="(user,index) in users">
+        <v-list-tile :key="user">
+          <v-list-tile-action>
+            <v-checkbox
+              v-model="selected"
+              :value="user"/>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-text-field
+              v-model="users[index]"
+              :value="user"/>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider
+          v-if="index + 1 < users.length"
+          :key="`user-divider-${index}`"/>
+      </template>
+    </v-list>
+
+
     <v-btn :to="resultUtl">結果作成</v-btn>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      users: [],
-      selected: [],
-      array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      descriptionFlag: true
-    };
-  },
-  asyncData(context) {
-    let descriptionFlag = context.query["users"] == null ? true : false;
-    let splitUsers;
-    if (!descriptionFlag) {
-      splitUsers = context.query["users"].split(",");
-    }
-
-    return {
-      users: splitUsers,
-      descriptionFlag: descriptionFlag
-    };
-  },
-  computed: {
-    resultUtl() {
-      let users = "users=" + this.selected;
-      // TODO sheed値の桁数などは要検討
-      let seed = "seed=" + Math.random();
-
-      return "/result?" + users + "&" + seed;
-    }
-  },
-  methods: {
-    shuffle() {
-      console.log("called");
-
-      for (let i = this.array.length - 1; i >= 0; i--) {
-        // 0~iのランダムな数値を取得
-        let rand = Math.floor(Math.random() * (i + 1));
-
-        // 配列の数値を入れ替える
-        [this.array[i], this.array[rand]] = [this.array[rand], this.array[i]];
+  export default {
+    data() {
+      return {
+        users: [],
+        selected: [],
+        array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        descriptionFlag: true
+      };
+    },
+    asyncData(context) {
+      let descriptionFlag = context.query["users"] == null ? true : false;
+      let splitUsers;
+      if (!descriptionFlag) {
+        splitUsers = context.query["users"].split(",");
       }
 
-      //TODO シャッフルする対象の配列の修正
-      console.log(this.array);
+      return {
+        users: splitUsers,
+        selected: splitUsers,
+        descriptionFlag: descriptionFlag
+      };
+    },
+    computed: {
+      resultUtl() {
+        let users = "users=" + this.selected;
+        // TODO sheed値の桁数などは要検討
+        let seed = "seed=" + Math.random();
+
+        return "/result?" + users + "&" + seed;
+      }
+    },
+    methods: {
+      addUser() {
+        this.users.push("");
+      }
     }
-  }
-};
+  };
 </script>
