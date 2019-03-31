e1d41cfc1fa5d190834a626d9976dd665b01e0bb
nannany <ymym1990ymym@gmail.com>
2019-03-31 23:47:39 +0900

    usersからの時に+押したときの挙動修正。

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <dev v-if="descriptionFlag">
-      Please add query parameter "users" like
-      <br>http://localhost:3000/?users=nannany,platykun
-    </dev>
+    <dev v-if="descriptionFlag">Please add user by clicking following plus button.</dev>
     <br>
     <v-btn fab dark color="info">
       <v-icon dark @click="addUser">add</v-icon>
@@ -13,64 +10,57 @@
       <template v-for="(user,index) in users">
         <v-list-tile :key="user">
           <v-list-tile-action>
-            <v-checkbox
-              v-model="selected"
-              :value="user"/>
+            <v-checkbox v-model="selected" :value="user"/>
           </v-list-tile-action>
 
           <v-list-tile-content>
-            <v-text-field
-              v-model="users[index]"
-              :value="user"/>
+            <v-text-field v-model="users[index]" :value="user"/>
           </v-list-tile-content>
         </v-list-tile>
-        <v-divider
-          v-if="index + 1 < users.length"
-          :key="`user-divider-${index}`"/>
+        <v-divider v-if="index + 1 < users.length" :key="`user-divider-${index}`"/>
       </template>
     </v-list>
 
-
-    <v-btn :to="resultUtl">結果作成</v-btn>
+    <v-btn :to="resultUtl">check result</v-btn>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        users: [],
-        selected: [],
-        array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        descriptionFlag: true
-      };
-    },
-    asyncData(context) {
-      let descriptionFlag = context.query["users"] == null ? true : false;
-      let splitUsers;
-      if (!descriptionFlag) {
-        splitUsers = context.query["users"].split(",");
-      }
-
-      return {
-        users: splitUsers,
-        selected: splitUsers,
-        descriptionFlag: descriptionFlag
-      };
-    },
-    computed: {
-      resultUtl() {
-        let users = "users=" + this.selected;
-        // TODO sheed値の桁数などは要検討
-        let seed = "seed=" + Math.random();
-
-        return "/result?" + users + "&" + seed;
-      }
-    },
-    methods: {
-      addUser() {
-        this.users.push("");
-      }
+export default {
+  data() {
+    return {
+      users: [],
+      selected: [],
+      array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      descriptionFlag: true
+    };
+  },
+  asyncData(context) {
+    let descriptionFlag = context.query["users"] == null ? true : false;
+    let splitUsers = [];
+    if (!descriptionFlag) {
+      splitUsers = context.query["users"].split(",");
     }
-  };
+
+    return {
+      users: splitUsers,
+      selected: splitUsers,
+      descriptionFlag: descriptionFlag
+    };
+  },
+  computed: {
+    resultUtl() {
+      let users = "users=" + this.selected;
+      // TODO sheed値の桁数などは要検討
+      let seed = "seed=" + Math.random();
+
+      return "/result?" + users + "&" + seed;
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push("");
+    }
+  }
+};
 </script>
