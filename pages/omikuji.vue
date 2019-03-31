b34b83c2f4119870b57fd85e52bc1049759a54d6
platykun <nagonago561@yahoo.co.jp>
2019-03-31 15:31:08 +0900

    引数の値を配列で取得 乱数の仮実装

--- pages/omikuji.vue
+++ pages/omikuji.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     omikuji
-    {{ users }}
+    <br>
+    // TODO 複数選択できない
+    <v-container fluid>
+      <v-flex v-for="user in users" >
+        <v-checkbox v-model="selected" :label="user" :value="user">{{user}}</v-checkbox>
+      </v-flex>
+    </v-container>
+    <v-btn @click="shuffle">シャッフルボタン</v-btn>
   </div>
 </template>
 
@@ -9,15 +16,47 @@
   export default {
     data() {
       return {
-        users: "",
+        users: [],
+        selected: "",
+        selectedUsers: [],
+        array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
       };
     },
     asyncData(context) {
       // TODO context.query['users']をカンマ区切りで配列化
       // TODO 引数が空になった時には空文字
+      let splitUsers = context.query['users'].split(',');
+
       return {
-        users: context.query['users']
+        users: splitUsers,
       }
     },
+    watch: {
+      selected() {
+        this.selectedUsers = this.selected.split(',');
+      }
+    },
+    computed: {
+      randomValue() {
+        return Math.random();
+      },
+    },
+    methods: {
+      shuffle() {
+        console.log("called");
+
+        for (let i = this.array.length - 1; i >= 0; i--) {
+
+          // 0~iのランダムな数値を取得
+          let rand = Math.floor(Math.random() * (i + 1));
+
+          // 配列の数値を入れ替える
+          [this.array[i], this.array[rand]] = [this.array[rand], this.array[i]]
+        }
+
+        //TODO シャッフルする対象の配列の修正
+        console.log(this.array);
+      }
+    }
   }
 </script>
