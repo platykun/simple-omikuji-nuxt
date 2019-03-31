732667c71c2b3fbb7879f9f1bc8480ccd3749d47
platykun <nagonago561@yahoo.co.jp>
2019-03-31 14:10:28 +0900

    ファイル追加

---
+++ pages/omikuji.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    omikuji
+  </div>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+  import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+  export default {
+    components: {
+      Logo,
+      VuetifyLogo
+    }
+  }
+</script>
